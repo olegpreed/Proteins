64dddc3f29344f34ef3f270d8bb39668d9c2e3b3
Oleg <leo.silveri@mail.ru>
2025-08-04 16:21:56 +0700

    Initial Commit

---
+++ Proteins/ProteinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProteinsApp.swift
+//  Proteins
+//
+//  Created by Oleg on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ProteinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProteinsTests/ProteinsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProteinsTests.swift
+//  ProteinsTests
+//
+//  Created by Oleg on 8/4/25.
+//
+
+import Testing
+@testable import Proteins
+
+struct ProteinsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
